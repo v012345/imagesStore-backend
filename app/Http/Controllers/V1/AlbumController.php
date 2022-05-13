17b69779955b363b88ffd938af6569b2f2de5c95
v138468 <v138468@gmail.com>
2022-05-13 15:07:39 +0800

    test api

--- app/Http/Controllers/V1/AlbumController.php
+++ app/Http/Controllers/V1/AlbumController.php
@@ -49,7 +49,7 @@ class AlbumController extends Controller
         //
         $album = Album::find($id);
         if ($album) {
-            return  $album->images;
+            return  $album->images->paginate($request->per_page ?? 15);
         } else {
             return response("", 404);
         }
