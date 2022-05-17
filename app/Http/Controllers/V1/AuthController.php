9a6a8186f3ed77315ab1d88f5291d59baae97a93
v138468 <v138468@gmail.com>
2022-05-17 13:57:12 +0800

    i

---
+++ app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    //
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+        $success['email'] =  $user->email;
+        return response()->json($success, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\AlbumController;
+use App\Http\Controllers\V1\AuthController;
 use App\Http\Controllers\V1\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::prefix("v1")->group(function () {
+
+    Route::post("register", [AuthController::class, "signup"]);
+
     Route::apiResource('images', ImageController::class);
     Route::apiResource('albums', AlbumController::class);
 });
