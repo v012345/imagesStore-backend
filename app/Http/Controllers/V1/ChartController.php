78cc05e71e6e7ade4709463f414d133d71ab3c0c
v138468 <v138468@gmail.com>
2022-05-19 17:09:15 +0800

    tet

--- app/Http/Controllers/V1/ChartController.php
+++ app/Http/Controllers/V1/ChartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
@@ -13,7 +14,11 @@ class ChartController extends Controller
         /** @var \App\Models\User $user */
         $user = auth()->user();
         $albums = $user->albums()->withCount("images")->orderByDesc("id")->limit(3)->get();
-        $images = $user->albums->images()->get();
-        return ["albums" => $albums, "images" => $images];
+        $cdn = Image::where("has_uploaded_to_cdn", true)->sum("size");
+        $local = Image::where("has_uploaded_to_cdn", false)->sum("size");
+        return ["albums" => $albums, "images" => [
+            "cdn" => $cdn,
+            "local" => $local,
+        ]];
     }
 }
