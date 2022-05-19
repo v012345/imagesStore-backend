a2bfd14f5e4d90b2f32fb1db10b2f9c51a18308a
Meteor <v138468@gmail.com>
2022-05-19 19:11:59 +0800

    test

--- app/Http/Controllers/V1/AlbumController.php
+++ app/Http/Controllers/V1/AlbumController.php
@@ -19,7 +19,7 @@ class AlbumController extends Controller
         /** @var \App\Models\User $user */
         $user = auth()->user();
         return $user->albums()->with("images", function ($query) {
-            // $query->limit(1);
+            $query->limit(1);
         })->withCount("images")->orderByDesc("id")->get();
     }
 
